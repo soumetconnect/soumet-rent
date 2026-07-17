e73cbd564b73755cebde4d748d7e00efb721cc38
Oghale Elebe <soumetconnect@gmail.com>
2026-07-17 14:24:29 +0100

    Initial commit for Rent Tracker

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rent_tracker.db'
+db = SQLAlchemy(app)
+
+class Tenant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+    building_loc = db.Column(db.String(100))
+    town = db.Column(db.String(100))
+    apt_type = db.Column(db.String(50))
+    rent_total = db.Column(db.Float)
+    amount_paid = db.Column(db.Float)
+    move_in_date = db.Column(db.Date)
+    expiry_date = db.Column(db.Date)
+
+    @property
+    def outstanding(self):
+        return self.rent_total - self.amount_paid
+
+@app.route('/')
+def index():
+    return render_template('index.html', tenants=Tenant.query.all())
+
+@app.route('/add', methods=['POST'])
+def add():
+    t = Tenant(
+        name=request.form['name'], phone=request.form['phone'],
+        building_loc=request.form['loc'], town=request.form['town'],
+        apt_type=request.form['type'], rent_total=float(request.form['total']),
+        amount_paid=float(request.form['paid']),
+        move_in_date=datetime.strptime(request.form['move'], '%Y-%m-%d'),
+        expiry_date=datetime.strptime(request.form['exp'], '%Y-%m-%d')
+    )
+    db.session.add(t)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context(): db.create_all()
+    app.run(host='0.0.0.0', port=8080, debug=True)
